aa635ee47ef978ac8f3af179b1d67f3060578ce9
Bhavin Dholakiya <bhavin.dholakiya23@gmail.com>
2023-11-29 18:51:27 +0530

    Add product as per ID fetch from all products.

--- src/main/java/org/testcase/Basics.java
+++ src/main/java/org/testcase/Basics.java
@@ -2,18 +2,16 @@ package main.java.org.testcase;
 
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
-import org.testng.Assert;
-
-import java.util.List;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class Basics {
 
+    JsonPath js;
+
     public static void main(String[] args) {
 
-        int ExpectedQuantity = 3;
         // validating Add Cart API is working as expected.
 
         /*
@@ -22,11 +20,37 @@ public class Basics {
          * then: where we are validation Response code, Response message, Response header, Response body
          * */
 
+        int expectedQuantity = 3;
+        String expectedProductTitle = "Brown Perfume";
+        int productID = 0;
+
         RestAssured.baseURI= "https://dummyjson.com/";
+        Basics obj = new Basics();
 
         // Get all the products
+        JsonPath js = new JsonPath(obj.GetAllProducts());
+        int NoOfProduct = js.get("products.size()");
+
+        String titleFirstProduct = js.get("products[0].title");
+        System.out.println("First Product is: " +titleFirstProduct);
+
+        // Get ID of an item and then pass to ID to next request for cart
+        for(int i=0; i<NoOfProduct; i++){
+            String productTitle = js.getString("products["+i+"].title");
+            if(productTitle.equalsIgnoreCase(expectedProductTitle)){
+                productID = js.getInt("products["+i+"].id");
+                break;
+            }
+        }
+
+        // Add products in Cart by ID and quantity
+        obj.AddProductToCart(productID);
+
+    }
+
+    public String GetAllProducts(){
         String productList = given()
-                    .log().all().header("Content-Type", "application/json")
+                    .header("Content-Type", "application/json")
                 .when()
                     .get("/products")
                 .then()
@@ -35,44 +59,34 @@ public class Basics {
 
         // Print all the items name
         JsonPath ProList = new JsonPath(productList);
-        List<String> itemNames = ProList.get("products.title");
+        int ProductsCount = ProList.getInt("products.size()");
+        System.out.println("No. of products: " +ProductsCount);
 
-        for(String name:itemNames){
-            System.out.println(name);
-        }
+        return productList;
+    }
 
-        // Get ID of an item and then pass to ID to next request for cart
-
-        // Add products in Cart by ID and quantity
+    public void AddProductToCart(int productID){
         String response = given()
                     .log().all()
                     .header("Content-Type", "application/json")
-                    .body(AddCart())
+                    .body(AddCart(productID))
                 .when()
                     .post("carts/add")
                 .then()
                     .log().all()
-                    .assertThat().statusCode(200).body("total", equalTo(621))
+                    .assertThat().statusCode(200)
+                    //.body("total", equalTo(621))
                     .header("server", "railway").extract().response().asString();
-
-        JsonPath js = new JsonPath(response);
-        int ActualQuantity = js.getInt("totalQuantity");
-
-        Assert.assertEquals(ActualQuantity, ExpectedQuantity);
     }
 
 
-    public static String AddCart(){
+    public static String AddCart(int productID){
         return "{\n" +
                 "    \"userId\": 1,\n" +
                 "    \"products\": [\n" +
                 "        {\n" +
-                "            \"id\": 1,\n" +
+                "            \"id\": \""+productID+"\",\n" +
                 "            \"quantity\": 1\n" +
-                "        },\n" +
-                "        {\n" +
-                "            \"id\": 50,\n" +
-                "            \"quantity\": 2\n" +
                 "        }\n" +
                 "    ]\n" +
                 "}";
